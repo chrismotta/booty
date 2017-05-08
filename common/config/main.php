6df6a7a05ef8c24818a2653b97e2a80614c9f19f
krazydanny <danielspadafora@gmail.com>
2017-05-08 16:02:29 -0300

    Added MySQL data to Main.php

--- common/config/main.php
+++ common/config/main.php
@@ -2,8 +2,17 @@
 return [
     'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
     'components' => [
+        'db' => [
+            'class' => 'yii\db\Connection',
+            'dsn' => 'mysql:host=phatt-cluster-1.cluster-cch7ui9gbr3f.us-east-1.rds.amazonaws.com;dbname=phatt',
+            'username' => 'root',
+            'password' => 'th3m3d14l4b',
+            'charset' => 'utf8',
+        ],    
+    	/*
         'cache' => [
             'class' => 'yii\caching\FileCache',
         ],
+        */
     ],
 ];
