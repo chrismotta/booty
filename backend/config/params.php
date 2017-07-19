f5de6922bc74080051cb348cafb0e7fd89994edb
krazydanny <danielspadafora@gmail.com>
2017-07-19 17:07:16 -0300

    fix

--- backend/config/params.php
+++ backend/config/params.php
@@ -1,5 +1,5 @@
 <?php
 return [
     'adminEmail' => 'admin@example.com',
-    'predisConString'	=> 'tcp://aff-redis.nktz2l.0001.use1.cache.amazonaws.com:6379'
+    'predisConString'	=> 'tcp://scabb.je8ge2.0001.use1.cache.amazonaws.com:6379'
 ];
