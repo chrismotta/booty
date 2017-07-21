4195bfc24fdd05231009f367a5cb23ad957983aa
Hipercubus <subespacios@gmail.com>
2017-07-21 11:21:31 -0300

    cluster assignment testing

--- backend/views/layouts/header.php
+++ backend/views/layouts/header.php
@@ -7,7 +7,7 @@ use yii\helpers\Html;
 
 <header class="main-header">
 
-    <?= Html::a('<span class="logo-mini">SPD</span><span class="logo-lg">' . Yii::$app->name . '</span>', Yii::$app->homeUrl, ['class' => 'logo']) ?>
+    <?= Html::a('<span class="logo-mini"><img src="'.Yii::$app->homeUrl.'/img/splad-iso.png" style="vertical-align: top; width: 38px; margin-top: 3px;" /></span><span class="logo-lg"><img src="'.Yii::$app->homeUrl.'/img/splad-logo.png" style="vertical-align: top; width: 120px; margin-top: 3px;" /></span>', Yii::$app->homeUrl, ['class' => 'logo']) ?>
 
     <nav class="navbar navbar-static-top" role="navigation">
 
