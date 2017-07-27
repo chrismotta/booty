737d8d27ea64e2a954f12f0fce17369b7aec692c
krazydanny <danielspadafora@gmail.com>
2017-07-27 16:07:00 -0300

    Dashboard deploy

--- backend/views/site/index.php
+++ backend/views/site/index.php
@@ -93,49 +93,19 @@ $byCountry = $byCountryProvider->getModels();
             </ul>
             <div class="tab-content no-padding">
               <!-- Morris chart - Sales -->
-              <div class="chart tab-pane active" id="revenue-chart" style="position: relative; height: 300px;">
                   <canvas id="myChart" width="400" height="400"></canvas>
                   <script>
                     var ctx = document.getElementById("myChart").getContext('2d');
-                    var myChart = new Chart(ctx, {
-                        type: 'bar',
-                        data: {
-                            labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-                            datasets: [{
-                                label: '# of Votes',
-                                data: [12, 19, 3, 5, 2, 3],
-                                backgroundColor: [
-                                    'rgba(255, 99, 132, 0.2)',
-                                    'rgba(54, 162, 235, 0.2)',
-                                    'rgba(255, 206, 86, 0.2)',
-                                    'rgba(75, 192, 192, 0.2)',
-                                    'rgba(153, 102, 255, 0.2)',
-                                    'rgba(255, 159, 64, 0.2)'
-                                ],
-                                borderColor: [
-                                    'rgba(255,99,132,1)',
-                                    'rgba(54, 162, 235, 1)',
-                                    'rgba(255, 206, 86, 1)',
-                                    'rgba(75, 192, 192, 1)',
-                                    'rgba(153, 102, 255, 1)',
-                                    'rgba(255, 159, 64, 1)'
-                                ],
-                                borderWidth: 1
-                            }]
-                        },
-                        options: {
-                            scales: {
-                                yAxes: [{
-                                    ticks: {
-                                        beginAtZero:true
-                                    }
-                                }]
-                            }
-                        }
+                    var myLineChart = new Chart(ctx, {
+                        type: 'line',
+                        data: [ 10, 20, 30, 40 ],
+                        options: []
                     });
-                  </script>                
+                  </script>               
+              <div class="chart tab-pane active" id="revenue-chart" style="position: relative; height: 300px;">               
               </div>
-              <div class="chart tab-pane" id="sales-chart" style="position: relative; height: 300px;"></div>
+              <div class="chart tab-pane" id="sales-chart" style="position: relative; height: 300px;">
+              </div>
             </div>
           </div>
           <!-- /.nav-tabs-custom -->
