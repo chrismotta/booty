9b0a038268db21a7b0ca2a328905ea4c34a0a5bd
Hipercubus <subespacios@gmail.com>
2017-08-10 12:40:46 -0300

    design changes

--- backend/views/affiliates/view.php
+++ backend/views/affiliates/view.php
@@ -13,7 +13,7 @@ $this->params['breadcrumbs'][] = $this->title;
 <div class="affiliates-view">
 
     <p>
-        <?= Html::a('Admin', ['affilliates/'], ['class' => 'btn btn-success']) ?>
+        <?= Html::a('Admin', ['affiliates/'], ['class' => 'btn btn-success']) ?>
         <?= Html::a('Update', ['update', 'id' => $model->id], ['class' => 'btn btn-primary']) ?>
         <?= Html::a('Delete', ['delete', 'id' => $model->id], [
             'class' => 'btn btn-danger',
