b41bf643c2504d3e52015799c528d6544190b164
Hipercubus <subespacios@gmail.com>
2017-08-14 14:43:29 -0300

    ext_id

--- backend/views/campaigns/view.php
+++ backend/views/campaigns/view.php
@@ -32,6 +32,7 @@ $this->params['breadcrumbs'][] = $this->title;
                 'attribute' => 'affiliates.name'
             ],
             'name',
+            'ext_id',
             'payout',
             'country',
             'connection_type',
