2e17e1088dcb228e0146739e3968780a7c8fe779
Hipercubus <subespacios@gmail.com>
2017-10-03 16:12:43 -0300

    maintenance

---
+++ backend/views/site/maintenance.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Splad under maintenance</title>
+    <style>
+        body {margin:0; padding:0; }
+        .footer {   
+            position:fixed;
+            left:0px;
+            bottom:0px;
+            height:100%;
+            width:100%;
+            z-index:1;
+        }
+        .header {   
+            position:fixed;
+            left:10px;
+            top:10px;
+            z-index:2;
+        }
+    </style>
+</head>
+<body>
+    <img class="header" src="img/splad-logo.png"/>
+    <img class="footer" src="img/maintenance.jpg"/>
+</body>
+</html>
