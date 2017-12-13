16ce5f0133debe7c6ca05b70d9d32566b0edee21
Hipercubus <subespacios@gmail.com>
2017-12-13 10:37:53 -0300

    debuging freq error

--- backend/controllers/ClustersController.php
+++ backend/controllers/ClustersController.php
@@ -445,12 +445,12 @@ class ClustersController extends Controller
             {
                 foreach ( $packageIds as $packageId )
                 {
-                    $c = $Clusters_id;
-                    $d = $delivery_freq;
-                    $i = $chc->campaigns->id;
-                    $a = $chc->campaigns->affiliates->id;
-                    $p = $packageId;
-                    //$cache->zadd( 'clusterlist:'.$Clusters_id, $delivery_freq, $chc->campaigns->id.':'.$chc->campaigns->affiliates->id.':'.$packageId );
+                    // $c = $Clusters_id;
+                    // $d = $delivery_freq;
+                    // $i = $chc->campaigns->id;
+                    // $a = $chc->campaigns->affiliates->id;
+                    // $p = $packageId;
+                    $cache->zadd( 'clusterlist:'.$Clusters_id, $delivery_freq, $chc->campaigns->id.':'.$chc->campaigns->affiliates->id.':'.$packageId );
                 }
             }
         }
